1473269e6deb4e86574bf0ebd069a75c7edc91b5
Dipankar <dipankar15-11137@diu.edu.bd>
2022-05-23 18:56:57 +0600

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Dashboard from './Pages/Dashboard/Dashboard';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Share/Navbar';
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Share/Navbar.js
+++ src/Pages/Share/Navbar.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
 const Navbar = () => {
 
-    const menuItems = <>
-        <Link to='/'><li>Home</li></Link>
 
-        <li>Home</li>
-        <li>Home</li>
-        <li>Home</li>
+    const menuItems = <>
+        <li className='text-xl font-bold'><Link to="/">Home</Link></li>
+        <li className='text-xl font-bold'><Link to="/">About</Link></li>
+        <li className='text-xl font-bold' ><Link to="/">My Orders</Link></li>
+        <li className='text-xl font-bold' ><Link to="/">Add A Review</Link></li>
+        <li className='text-xl font-bold' ><Link to="/">My Profile</Link></li>
+        <li className='text-xl font-bold' ><Link to="/dashboard">Dashboard</Link></li>
+
     </>
 
     return (
-        <div class="navbar bg-base-100">
-            <div class="navbar-start">
-                <div class="dropdown">
-                    <label tabindex="0" class="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <div >
+            <div className="navbar ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItems}
+                        </ul>
+                    </div>
+                    <Link to='/' className="btn btn-ghost normal-case font-bold text-2xl text-primary">Manufacture Tools</Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
                         {menuItems}
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">Manufacture Tools</a>
+                <div className="navbar-end">
+                    <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                </div>
             </div>
-            <div class="navbar-center hidden lg:flex">
-                {menuItems}
-            </div>
-
         </div>
     );
 };
 
 export default Navbar;
+
